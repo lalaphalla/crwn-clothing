57aba5b7e6b1e2ce257232e5395c8836710389af
Lalaphalla <lalaphalla@gmail.com>
2020-06-18 20:48:30 +0700

    create project with create react app and added files for homepage organize file struture and add background-image

---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './homepage.style.scss'
+import Directory from '../../directory/directory.compoent'
+const HomePage = () =>(
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+export default HomePage;
